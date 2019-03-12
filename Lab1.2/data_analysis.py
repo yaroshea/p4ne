3d24964f50911b21495e727176a45493a77d1df9
yaroshea <ea.yarosh@jet.su>
2019-03-12 17:13:11 +0300

    Worked lab

---
+++ Lab1.2/data_analysis.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+
+
+def getvalue(x):
+    return x.value
+
+list_x = list(map(getvalue, sheet['A'][1:]))
+list_y = list(map(getvalue, sheet['C'][1:]))
+list_z = list(map(getvalue, sheet['D'][1:]))
+
+
+
+pyplot.plot(list_x, list_y, label='Метка')
+pyplot.plot(list_x, list_z, label='Метка')
+pyplot.show()
