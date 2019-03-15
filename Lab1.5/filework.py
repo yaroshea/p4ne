76bd261eb56787048b4de48c94998c5c866e2944
yaroshea <ea.yarosh@jet.su>
2019-03-15 17:18:11 +0300

    Worked lab

--- Lab1.5/filework.py
+++ Lab1.5/filework.py
@@ -1,9 +1,11 @@
 import os
+# путь к директории с файлами
 p = "C:/Users/ea.yarosh/Seafile/p4ne_training/config_files/"
-files = os.listdir(path="C:/Users/ea.yarosh/Seafile/p4ne_training/config_files")
+
+files = os.listdir(path=p)
 fall = []
 for i in files:
-    file = open("C:/Users/ea.yarosh/Seafile/p4ne_training/config_files/" + i)
+    file = open(p + i)
     for s in file:
         if s.strip().lower().startswith("ip address"):
             f = s.strip().lower().replace("ip address", "")
