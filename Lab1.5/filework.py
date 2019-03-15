418ee27fcf5c82a6006caef250962295b51af42d
yaroshea <ea.yarosh@jet.su>
2019-03-15 17:17:02 +0300

    Worked lab

--- Lab1.5/filework.py
+++ Lab1.5/filework.py
@@ -1,11 +1,14 @@
 import os
-
+p = "C:/Users/ea.yarosh/Seafile/p4ne_training/config_files/"
 files = os.listdir(path="C:/Users/ea.yarosh/Seafile/p4ne_training/config_files")
 fall = []
 for i in files:
     file = open("C:/Users/ea.yarosh/Seafile/p4ne_training/config_files/" + i)
     for s in file:
-        if s.strip().lower().startswith("ip address: "):
-            fall.append(s.strip().lower().replace("ip address: ", ""))
+        if s.strip().lower().startswith("ip address"):
+            f = s.strip().lower().replace("ip address", "")
+            if not f.strip().startswith(":"): fall.append(f.strip())
+            else: fall.append(f.strip().replace(": ", ""))
+
 print(set(fall))
 
